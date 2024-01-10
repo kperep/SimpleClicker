a4f8bfe4daf72d871888e0d07ce4535904c42d1d
Sergey Usachev <kperep@gmail.com>
2024-01-10 17:32:50 +0700

    Bug fixes

--- main.py
+++ main.py
@@ -6,39 +6,43 @@ def clicker():
 
     # функция прибавления кликов
     def click(event):
-        click_count.configure(state="normal")
-        x = int(click_count.get())
-        x += int(power_label_amount['text'])
-        click_count.delete(0, tk.END)
-        click_count.insert(0, str(x))
-        click_count.configure(state="readonly")
+        click_count.configure(state="normal")   # разрешаем изменять поле с количеством кликов
+        x = int(click_count.get())              # получаем текущее количество кликов
+        x += int(power_label_amount['text'])    # увеличиваем его на значение мощности
+        click_count.delete(0, tk.END)       # очищаем поле с количеством кликов
+        click_count.insert(0, str(x))      # вставляем туда новое значение
+        click_count.configure(state="readonly") # запрещаем редактирование поля с количеством кликов
 
     # функция покупки увеличения мощности
     def buy_item_1(event):
-        amount = int(click_count.get())
-        cost = int(shop_item_1_cost['text'].split()[0])
-        if amount >= cost:
-            click_count.configure(state="normal")
-            power = int(power_label_amount['text'])
-            power *= 5
-            power_label_amount.configure(text=str(power))
-            amount -= cost
-            click_count.delete(0, tk.END)
-            click_count.insert(0, str(amount))
-            cost *= 10
-            shop_item_1_cost.configure(text=str(cost) + " кликов")
-            click_count.configure(state="readonly")
+        amount = int(click_count.get())                     # получаем количество кликов
+        cost = int(shop_item_1_cost['text'].split()[0])     # получаем стоимость товара
+        if amount >= cost:          # если денег хватает
+            click_count.configure(state="normal")           # разрешаем изменять поле с количеством кликов
+            power = int(power_label_amount['text'])         # считываем текущее значение мощности
+            power *= 5                                      # увеличиваем текущее значение мощности
+            power_label_amount.configure(text=str(power))   # меняем старое значение мощности на новое
+            amount -= cost          # забираем у пользователя стоимость товара
+            click_count.delete(0, tk.END)               # очищаем количество кликов
+            click_count.insert(0, str(amount))         # вставляем новое значение кликов
+            cost *= 10                                      # увеличиваем стоимость следующего апгрейда в 10 раз
+            shop_item_1_cost.configure(text=str(cost) + " кликов")  # меняем цену на новую
+            click_count.configure(state="readonly")         # запрещаем редактирование поля с количеством кликов
 
     # функция покупки фона
     def buy_item_2(event):
-        amount = int(click_count.get())
-        if amount >= 1000000:
-            click_count.configure(state="normal")
-            window.config(bg="gold")
-            amount -= 1000000
-            click_count.delete(0, tk.END)
-            click_count.insert(0, str(amount))
-            click_count.configure(state="readonly")
+        amount = int(click_count.get())                     # получаем количество кликов
+        if amount >= 1000000:                               # если денег хватает
+            click_count.configure(state="normal")           # разрешаем редактировать поле с количеством кликов
+            window.config(bg="gold")                        # меняем фон
+            amount -= 1000000                               # отнимаем у пользователя стоимость апгрейда
+            # отключаем возможность повторной покупки
+            shop_item_2_buy.destroy()
+            shop_item_2_cost.destroy()
+            shop_item_2_label.destroy()
+            click_count.delete(0, tk.END)               # очищаем количество кликов
+            click_count.insert(0, str(amount))         # добавляем новое значение кликов
+            click_count.configure(state="readonly")         # запрещаем редактирование поля с количеством кликов
 
     window = tk.Tk()                    # создаём главную форму
     window.title("UberClicker")         # заголовок окна
