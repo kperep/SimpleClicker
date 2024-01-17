04d6e73fbe3024a5d21a635a57cd825208a4f0f8
Sergey Usachev <kperep@gmail.com>
2024-01-17 17:03:11 +0700

    Add some design

--- main.py
+++ main.py
@@ -46,47 +46,47 @@ def clicker():
 
     window = tk.Tk()                    # создаём главную форму
     window.title("UberClicker")         # заголовок окна
-    window.geometry('400x300')          # размер окна
+    window.geometry('440x300')          # размер окна
     window.resizable(False, False)   # запрет на растягивание
     ### форматирование формы игры ###
-    main_label = tk.Label(window, text="Кликай пока кликается!")
-    main_label.grid(row=0, column=0, columnspan=3)
-    clicks_label = tk.Label(window, text="Количество кликов:")
+    main_label = tk.Label(window, text="Кликай пока кликается!\n", font=('Lucida Sans', 20))
+    main_label.grid(row=0, column=0, columnspan=5)
+    clicks_label = tk.Label(window, text="Количество кликов:", font=('Lucida Sans', 10))
     clicks_label.grid(row=1, column=0)
-    click_count = tk.Entry(window, width=10)
+    click_count = tk.Entry(window, width=10, font=('Lucida Sans', 10))
     click_count.grid(row=1, column=1)
     click_count.insert(0, "0")
     click_count.configure(state="readonly")
     click_button = tk.Button(window, text="Кликнуть!")
     click_button.grid(row=1, column=2)
     ### форматирование формы магазина ###
-    shop_label = tk.Label(window, text="Магазин")
-    shop_label.grid(row=2, column=0, columnspan=3)
+    shop_label = tk.Label(window, text="\n\nМагазин\n", font=("Lucida Sans", 10, "bold"), fg="MediumSlateBlue")
+    shop_label.grid(row=2, column=0, columnspan=5)
     # Первый продукт
-    shop_item_1_label = tk.Label(window, text="Мощность клика x5")
-    shop_item_1_label.grid(row=3, column=0)
+    shop_item_1_label = tk.Label(window, text="Мощность клика x5", font=("Lucida Sans", 10))
+    shop_item_1_label.grid(row=3, column=0, sticky="W")
     item_1_cost = 10
-    shop_item_1_cost = tk.Label(window, text=str(item_1_cost) + " кликов")
-    shop_item_1_cost.grid(row=3, column=1)
+    shop_item_1_cost = tk.Label(window, text=str(item_1_cost) + " кликов", font=("Lucida Sans", 10), fg="Green")
+    shop_item_1_cost.grid(row=3, column=1, sticky="E")
     shop_item_1_buy = tk.Button(window, text="Купить!")
-    shop_item_1_buy.grid(row=3, column=2)
+    shop_item_1_buy.grid(row=3, column=2, columnspan=3)
     # Второй продукт
-    shop_item_2_label = tk.Label(window, text="Сменить фон")
-    shop_item_2_label.grid(row=4, column=0)
+    shop_item_2_label = tk.Label(window, text="\nСменить фон", font=("Lucida Sans", 10))
+    shop_item_2_label.grid(row=4, column=0, sticky="W")
     item_2_cost = 1000000
-    shop_item_2_cost = tk.Label(window, text=str(item_2_cost) + " кликов")
-    shop_item_2_cost.grid(row=4, column=1)
+    shop_item_2_cost = tk.Label(window, text="\n" + str(item_2_cost) + " кликов", font=("Lucida Sans", 10), fg="Green")
+    shop_item_2_cost.grid(row=4, column=1, sticky="E")
     shop_item_2_buy = tk.Button(window, text="Купить!")
-    shop_item_2_buy.grid(row=4, column=2)
+    shop_item_2_buy.grid(row=4, column=2, columnspan=3)
     # функционал кнопок
     click_button.bind("<Button-1>", click)              # фарм кликов
     shop_item_1_buy.bind("<Button-1>", buy_item_1)      # прокачка кликов
     shop_item_2_buy.bind("<Button-1>", buy_item_2)      # покупка фона
     # показатель мощности
-    power_label = tk.Label(window, text="Мощность")
-    power_label.grid(row=0, column=3)
-    power_label_amount = tk.Label(window, text="1")
-    power_label_amount.grid(row=1, column=3)
+    power_label = tk.Label(window, text="\tPower", font=('Lucida Sans', 10))
+    power_label.grid(row=1, column=3)
+    power_label_amount = tk.Label(window, text="1", font=('Lucida Sans', 10))
+    power_label_amount.grid(row=1, column=4)
 
     window.mainloop()
 
